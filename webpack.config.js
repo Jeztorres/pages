36290ec5c93a3a1231bdd02ec85dbd2fe51d0328
Jeztorres <l21200651@pachuca.tecnm.mx>
2025-10-08 03:02:44 -0600

    Add working demo version with functional menu buttons

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,50 @@
-const path = require('path');
-const pkg = require('./package.json');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const buildPath = './build/';
+const path = require("path");
+const pkg = require("./package.json");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const buildPath = "./dist/";
 
 module.exports = {
-  entry: ['./src/entry.js'],
+  entry: ["./src/entry.js"],
   output: {
     path: path.join(__dirname, buildPath),
-    filename: '[name].[fullhash].js',
-    publicPath: '',
+    filename: "[name].[fullhash].js",
+    publicPath: "",
   },
-  mode: 'development',
-  target: 'web',
-  devtool: 'source-map',
+  mode: "development",
+  target: "web",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: path.resolve(__dirname, './node_modules/')
+        use: "babel-loader",
+        exclude: path.resolve(__dirname, "./node_modules/"),
       },
       {
         test: /\.(jpe?g|png|gif|svg|tga|glb|gltf|bin|fbx|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg|wav)$/i,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-              esModule: false
-          }
+            esModule: false,
+          },
         },
-        exclude: path.resolve(__dirname, './node_modules/')
+        exclude: path.resolve(__dirname, "./node_modules/"),
       },
-    ]
+    ],
   },
   resolve: {
     alias: {
-      vendor: path.resolve(__dirname, 'vendor')
+      vendor: path.resolve(__dirname, "vendor"),
     },
     fallback: {
-      'fs': false,
-      'path': false, // ammo.js seems to also use path
-    }
+      fs: false,
+      path: false, // ammo.js seems to also use path
+    },
   },
   plugins: [
-    new HtmlWebpackPlugin({'title': 'Gers of War', template: './src/index.html'})
-  ]
-}
+    new HtmlWebpackPlugin({
+      title: "Zona de Combate",
+      template: "./index.html",
+    }),
+  ],
+};
